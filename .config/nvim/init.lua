446f22109787993ef4ae96c6915576fba93154cb
yskszk63 <yskszk63@gmail.com>
2022-03-02 21:06:15 +0900

    add pyright

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -470,11 +470,11 @@ _G.setup_lsp = function()
         }
     }
 
-  nvim_lsp.pyright.setup{
-      on_attach = on_attach,
-      capabilities = capabilities,
-      flags = {debounce_text_changes = 150},
-  }
+    nvim_lsp.pyright.setup{
+        on_attach = on_attach,
+        capabilities = capabilities,
+        flags = {debounce_text_changes = 150},
+    }
 
     -- https://github.com/neovim/nvim-lspconfig/wiki/UI-customization#change-diagnostic-symbols-in-the-sign-column-gutter
     local signs = {
