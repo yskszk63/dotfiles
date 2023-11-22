6a6957b9903cad3168bb848b658f949b5060fa3a
yskszk63 <yskszk63@gmail.com>
2023-11-22 09:14:06 +0900

    Update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -553,4 +553,11 @@ function lspconfig()
   nvim_lsp.kotlin_language_server.setup {
     capabilities = capabilities,
   }
+
+  nvim_lsp.apex_ls.setup {
+    capabilities = capabilities,
+    apex_jar_path = vim.fn.expand("${HOME}/.local/bin/apex-jorje-lsp.jar"),
+    apex_enable_semantic_errors = false, -- Whether to allow Apex Language Server to surface semantic errors
+    apex_enable_completion_statistics = false, -- Whether to allow Apex Language Server to collect telemetry on code completion usage
+  }
 end
