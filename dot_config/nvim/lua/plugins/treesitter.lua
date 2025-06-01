5ed2b3ac5d5d89110a4b8a6d96a29673d511d435
yskszk63 <yskszk63@gmail.com>
2025-06-01 12:44:14 +0900

    Update

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -6,6 +6,37 @@ return {
     build = ":TSUpdate",
     event = { 'BufRead', 'BufNewFile', 'InsertEnter' },
     config = function()
+      require'nvim-treesitter'.install {
+        "go",
+        "rust",
+        "javascript",
+        "typescript",
+        "tsx",
+      }
+
+      --vim.opt.foldenable = false
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = {
+          "go",
+          "rust",
+          "javascript",
+          "typescript",
+          "typescriptreact",
+        },
+        callback = function()
+          vim.treesitter.start()
+          vim.wo.foldtext = ""
+          vim.wo.foldmethod = "expr"
+          vim.wo.foldlevel = 99
+          vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end
+
+    --[==[
+    config = function()
       require'nvim-treesitter.configs'.setup {
         highlight = {
           enable = true,
@@ -16,5 +47,6 @@ return {
         },
       }
     end,
+    ]==]
   },
 }
