9118d28dd805e007c37153a828d3b14b1e12a5ba
yskszk63 <yskszk63@gmail.com>
2025-11-16 22:33:46 +0900

    jdtls settings

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,32 @@
+local function load_dropin_settings(server_name)
+  local ok, mod = pcall(require, "lsp.settings." .. server_name)
+  if not ok then
+    return nil
+  end
+  if type(mod) ~= "table" then
+    return nil
+  end
+  return mod
+end
+
+local function apply_dropin_settings(config)
+  local dropin = load_dropin_settings(config.name)
+
+  if dropin == nil then
+    return
+  end
+
+  if config.settings == nil then
+    config.settings = dropin
+  else
+    config.settings = vim.tbl_deep_extend(
+      "force",
+      config.settings,
+      dropin
+    )
+  end
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -56,6 +85,18 @@ return {
         filetypes = { "apexcode", "apex" },
       })
 
+      vim.lsp.config("jdtls", {
+      })
+
+      vim.lsp.config('*', {
+        before_init = function(_, config)
+          apply_dropin_settings(config)
+
+          local codesettings = require('codesettings')
+          config = codesettings.with_local_settings(config.name, config)
+        end,
+      })
+
       vim.lsp.enable({
         "gopls",
         "rust_analyzer",
@@ -80,7 +121,10 @@ return {
     dependencies = {
       --"simrat39/rust-tools.nvim",
       --"hrsh7th/cmp-nvim-lsp",
-      --"folke/neoconf.nvim",
+      --{ "folke/neoconf.nvim", cmd = "Neoconf", config = true },
+      { "mrjones2014/codesettings.nvim", opts = { jsonls_integration = true }, cmd = "Codesettings" },
+      "mfussenegger/nvim-jdtls",
+      --"nvim-java/nvim-java",
     },
   },
 }
