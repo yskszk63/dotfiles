940028a8733df3f9780dd5ae78718a60ac01c4e8
yskszk63 <yskszk63@gmail.com>
2026-01-06 23:35:14 +0900

    `deno:/`

---
+++ dot_config/nvim/after/ftplugin/typescript.lua
@@ -0,0 +1,42 @@
+-- https://github.com/denoland/deno/issues/28794#issuecomment-2888221811
+-- https://github.com/onion108/onion27-nvim-config/blob/a79cf9d5852561f12bc91147b6b6a0d7e455dad8/lua/terminal/tweaks/deno-lsp-correction.lua
+local function virtual_text_document(params)
+  --vim.notify("attempt to read: " .. vim.inspect(params.match))
+  local bufnr = params.buf
+  local actual_path = params.match:sub(1)
+
+  local clients = vim.lsp.get_clients { name = "denols" }
+  if #clients == 0 then
+    vim.notify("cannot find denols", vim.log.levels.WARN)
+    return
+  end
+
+  local client = clients[1]
+  local method = "deno/virtualTextDocument"
+  local req_params = { textDocument = { uri = actual_path } }
+  local response = client:request_sync(method, req_params, 2000, 0)
+  if not response or type(response.result) ~= "string" then
+    vim.notify("failed to get virtual document's content", vim.log.levels.WARN)
+    return
+  end
+
+  local lines = vim.split(response.result, "\n")
+  vim.bo[bufnr].modifiable = true -- ADD
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+  vim.api.nvim_set_option_value("readonly", true, { buf = bufnr })
+  vim.api.nvim_set_option_value("modified", false, { buf = bufnr })
+  vim.api.nvim_set_option_value("modifiable", false, { buf = bufnr })
+  vim.api.nvim_buf_set_name(bufnr, actual_path)
+  vim.lsp.buf_attach_client(bufnr, client.id)
+
+  local filetype = "typescript"
+  if actual_path:sub(-3) == ".md" then
+    filetype = "markdown"
+  end
+  vim.api.nvim_set_option_value("filetype", filetype, { buf = bufnr })
+end
+
+vim.api.nvim_create_autocmd({ "BufReadCmd" }, {
+  pattern = { "deno:/*", "asset://*" },
+  callback = virtual_text_document,
+})
--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -92,33 +92,33 @@ return {
     end
   },
 
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      lsp = {
-        progress = {
-          enabled = false,
-        },
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-        },
-      },
-      presets = {
-        --bottom_search = true,
-        --command_palette = true,
-        --long_message_to_split = true,
-        --inc_rename = false,
-        lsp_doc_border = true,
-      },
-    },
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      -- "rcarriga/nvim-notify",
-    },
-  },
+  -- {
+  --   "folke/noice.nvim",
+  --   event = "VeryLazy",
+  --   opts = {
+  --     lsp = {
+  --       progress = {
+  --         enabled = false,
+  --       },
+  --       override = {
+  --         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+  --         ["vim.lsp.util.stylize_markdown"] = true,
+  --         ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+  --       },
+  --     },
+  --     presets = {
+  --       --bottom_search = true,
+  --       --command_palette = true,
+  --       --long_message_to_split = true,
+  --       --inc_rename = false,
+  --       lsp_doc_border = true,
+  --     },
+  --   },
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --     -- "rcarriga/nvim-notify",
+  --   },
+  -- },
 
   {
     "folke/which-key.nvim",
