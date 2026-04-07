a1a59d157054b768295b412ee37af2eaf445a12c
yskszk63 <yskszk63@gmail.com>
2026-04-07 21:59:41 +0900

    Disable treesitter plugin

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,21 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    build = ":TSUpdate",
-    config = function ()
-      require("nvim-treesitter").setup({})
-      vim.api.nvim_create_autocmd("FileType", {
-        callback = function ()
-          if pcall(vim.treesitter.start) ~= true then
-            return
-          end
-          --vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end
-      })
-    end
-  },
-
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   lazy = false,
+  --   build = ":TSUpdate",
+  --   config = function ()
+  --     require("nvim-treesitter").setup({})
+  --     vim.api.nvim_create_autocmd("FileType", {
+  --       callback = function ()
+  --         if pcall(vim.treesitter.start) ~= true then
+  --           return
+  --         end
+  --         --vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  --       end
+  --     })
+  --   end
+  -- },
+  --
   {
     "nvim-treesitter/nvim-treesitter-context",
     config = true,
